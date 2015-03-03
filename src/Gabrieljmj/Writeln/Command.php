09b193b846bec0f36503bfcf0bf622a2d481cf58
Gabriel Jacinto <gamjj74@hotmail.com>
2015-03-03 15:46:24 -0300

    Add license

--- src/Gabrieljmj/Writeln/Command.php
+++ src/Gabrieljmj/Writeln/Command.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Writeln
+ *
+ * @author  Gabriel Jacinto <gamjj74@hotmail.com>
+ * @license MIT
+ * @link    https://github.com/GabrielJMJ/Writeln
+*/
+
 namespace Gabrieljmj\Writeln;
 
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
@@ -7,13 +15,29 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Command extends SymfonyCommand
 {
+    /**
+     * Command execution
+     *
+     * @var callable
+     */
     private $callback;
     
+    /**
+     * Sets the command execution
+     *
+     * @param callable $callback
+     */
     public function setCallback(callable $callback)
     {
         $this->callback = $callback;
     }
     
+    /**
+     * Executes the command
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface   $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         call_user_func($this->callback, $input, $output);
--- src/Gabrieljmj/Writeln/Writeln.php
+++ src/Gabrieljmj/Writeln/Writeln.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Writeln
+ *
+ * @author  Gabriel Jacinto <gamjj74@hotmail.com>
+ * @license MIT
+ * @link    https://github.com/GabrielJMJ/Writeln
+*/
+
 namespace Gabrieljmj\Writeln;
  
 use Gabrieljmj\Writeln\Command;
@@ -8,6 +16,14 @@ use Symfony\Component\Console\Input\InputArgument;
  
 class Writeln extends Application
 {
+    /**
+     * Creates a command
+     *
+     * @param string   $name
+     * @param array    $arguments
+     * @param array    $options
+     * @param callable $callback
+     */
     public function command($name, array $arguments, array $options, callable $callback)
     {
         $command = $this->createCommand($name, $arguments, $options);
@@ -15,6 +31,14 @@ class Writeln extends Application
         $this->add($command);
     }
  
+    /**
+     * Creates the command instance
+     *
+     * @param string   $name
+     * @param array    $arguments
+     * @param array    $options
+     * @return \Gabrieljmj\Writeln\Command
+     */
     private function createCommand($name, array $arguments, array $options)
     {
         $command = new Command($name);
@@ -30,11 +54,30 @@ class Writeln extends Application
         return $command;
     }
     
+    /**
+     * Adds a command argument
+     *
+     * @param string       $name
+     * @param integer|null $mode
+     * @param string       $description
+     * @param string|null  $default
+     * @return \Symfony\Component\Console\Input\InputArgument
+     */
     public function argument($name, $mode = null, $description = '', $default = null)
     {
         return new InputArgument($name, $mode, $description, $default);
     }
     
+    /**
+     * Adds a command option
+     *
+     * @param string       $name
+     * @param string|null  $shortcut
+     * @param integer|null $mode
+     * @param string       $description
+     * @param string|null  $default
+     * @return \Symfony\Component\Console\Input\InputOption
+     */
     public function option($name, $shortcut = null, $mode = null, $description = '', $default = null)
     {
         return new InputOption($name, $shortcut, $mode, $description, $default);
